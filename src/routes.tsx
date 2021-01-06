3dedbe2f22033fc9c81b06507f2c00c8327ec906
lll <www.547652855@qq.com>
2021-01-06 16:12:29 +0800

    feat:添加第一个路由

---
+++ src/routes.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+const Routes = () => <NavigationContainer>11</NavigationContainer>;
+
+export default Routes;
