da53fd83a1b7d145e0e37bb26d5c127c6b7f339a
lll <www.547652855@qq.com>
2021-01-06 16:17:59 +0800

    feat:优化路由

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
-import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
-const Routes = () => <NavigationContainer>11</NavigationContainer>;
+import {createStackNavigator} from '@react-navigation/stack';
+import Home from './module/home';
+
+const Stack = createStackNavigator();
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default Routes;
