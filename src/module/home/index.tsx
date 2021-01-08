3d44a758e973788df31f01cd55a0d090ffe989d7
lll <www.547652855@qq.com>
2021-01-08 10:43:07 +0800

    feat

--- src/module/home/index.tsx
+++ src/module/home/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View, Button, ScrollView, Dimensions} from 'react-native';
 import {Header} from 'react-native-elements';
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart,
+} from 'react-native-chart-kit';
+const screenWidth = Dimensions.get('window').width;
 
 interface IProps {
   navigation: any;
@@ -8,6 +17,98 @@ interface IProps {
 
 const Home = (props: IProps) => {
   const {navigation} = props;
+  const chartConfig = {
+    backgroundGradientFrom: '#1E2923',
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: '#08130D',
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+  const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+        color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+        strokeWidth: 2, // optional
+      },
+    ],
+    legend: ['Rainy Days'], // optional
+  };
+  const data1 = {
+    labels: ['Swim', 'Bike', 'Run'], // optional
+    data: [0.4, 0.6, 0.8],
+  };
+  const data2 = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+    datasets: [
+      {
+        data: [20, 45, 28, 80, 99, 43],
+      },
+    ],
+  };
+  const data3 = {
+    labels: ['Test1', 'Test2'],
+    legend: ['L1', 'L2', 'L3'],
+    data: [
+      [60, 60, 60],
+      [30, 30, 60],
+    ],
+    barColors: ['#dfe4ea', '#ced6e0', '#a4b0be'],
+  };
+  const data4 = [
+    {
+      name: 'Seoul',
+      population: 21500000,
+      color: 'rgba(131, 167, 234, 1)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Toronto',
+      population: 2800000,
+      color: '#F00',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Beijing',
+      population: 527612,
+      color: 'red',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'New York',
+      population: 8538000,
+      color: '#ffffff',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+    {
+      name: 'Moscow',
+      population: 11920000,
+      color: 'rgb(0, 0, 255)',
+      legendFontColor: '#7F7F7F',
+      legendFontSize: 15,
+    },
+  ];
+  const commitsData = [
+    {date: '2017-01-02', count: 1},
+    {date: '2017-01-03', count: 2},
+    {date: '2017-01-04', count: 3},
+    {date: '2017-01-05', count: 4},
+    {date: '2017-01-06', count: 5},
+    {date: '2017-01-30', count: 2},
+    {date: '2017-01-31', count: 3},
+    {date: '2017-03-01', count: 2},
+    {date: '2017-04-02', count: 4},
+    {date: '2017-03-05', count: 2},
+    {date: '2017-02-30', count: 4},
+  ];
   return (
     <>
       <Header
@@ -18,6 +119,106 @@ const Home = (props: IProps) => {
       <View>
         <Button title="openDrawer" onPress={() => navigation.openDrawer()} />
       </View>
+      <ScrollView>
+        <LineChart
+          data={{
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [
+              {
+                data: [
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                  Math.random() * 100,
+                ],
+              },
+            ],
+          }}
+          width={400} // from react-native
+          height={220}
+          yAxisLabel="$"
+          yAxisSuffix="k"
+          yAxisInterval={1} // optional, defaults to 1
+          chartConfig={{
+            backgroundColor: '#e26a00',
+            backgroundGradientFrom: '#fb8c00',
+            backgroundGradientTo: '#ffa726',
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+            propsForDots: {
+              r: '6',
+              strokeWidth: '2',
+              stroke: '#ffa726',
+            },
+          }}
+          bezier
+          style={{marginVertical: 8, borderRadius: 16}}
+        />
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+        <LineChart
+          data={data}
+          width={screenWidth}
+          height={256}
+          verticalLabelRotation={30}
+          chartConfig={chartConfig}
+          bezier
+        />
+        <ProgressChart
+          data={data1}
+          width={screenWidth}
+          height={220}
+          strokeWidth={16}
+          radius={32}
+          chartConfig={chartConfig}
+          hideLegend={false}
+        />
+        <BarChart
+          // style={graphStyle}
+          data={data2}
+          width={screenWidth}
+          height={220}
+          yAxisLabel="$"
+          chartConfig={chartConfig}
+          verticalLabelRotation={30}
+        />
+        <StackedBarChart
+          // style={graphStyle}
+          data={data3}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+        <PieChart
+          data={data4}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+          accessor={'population'}
+          backgroundColor={'transparent'}
+          paddingLeft={'15'}
+          center={[10, 50]}
+          absolute
+        />
+        <ContributionGraph
+          values={commitsData}
+          endDate={new Date('2017-04-01')}
+          numDays={105}
+          width={screenWidth}
+          height={220}
+          chartConfig={chartConfig}
+        />
+      </ScrollView>
     </>
   );
 };
